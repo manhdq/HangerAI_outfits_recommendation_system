8b58820776fc30bba3cd4292165cef755c51d73c
dungnq271 <dungnguyen2712002@gmail.com>
2023-12-18 23:56:54 +0700

    Save both embeddings and images

--- notebooks/embedding/embed-chatbot-images.py
+++ notebooks/embedding/embed-chatbot-images.py
@@ -16,8 +16,6 @@ from PIL import Image
 import cv2
 import matplotlib.pyplot as plt
 
-import torch
-
 from Hash4AllFashion_deploy.utils.param import FashionDeployParam
 from Hash4AllFashion_deploy.model import fashionnet
 from app import prediction
@@ -52,12 +50,16 @@ with open(config_path, "r") as f:
 config = FashionDeployParam(**kwargs)
 
 # %%
-model = prediction.Pipeline(config, storage_path)
+model = prediction.Pipeline(config, storage_path, False)
 model.net
 
 # %% [markdown]
-### Extract features and save to storage
+### Extract features and save to storage, save images to directory
 apparel_feature_vecs = {}
+
+# %%
+saved_imgs_dir = "../../data/chatbot_images"
+io.create_dir(saved_imgs_dir)
 
 # %%
 images = []
@@ -76,13 +78,14 @@ for idx, row in tqdm(df_full.iterrows()):
                 try:
                     image = np.frombuffer(content.read(), np.uint8)
                     image = cv2.imdecode(image, cv2.IMREAD_COLOR)
-                    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+                    # image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
                 except:
                     continue
             break
 
     # images.append(image)
 
+    # Extract features
     if image is not None:
         (
             lci_v,
@@ -97,6 +100,10 @@ for idx, row in tqdm(df_full.iterrows()):
             "bci_v": bci_v,
             "bci_s": bci_s,
         }
+        
+        # Save images
+        img_path = osp.join(saved_imgs_dir, item_id + f".jpg")
+        cv2.imwrite(img_path, image)
 
 # # %%
 # plt.imshow(random.sample(images, 1)[0])
@@ -106,17 +113,18 @@ print(len(apparel_feature_vecs))
 random.sample(apparel_feature_vecs.items(), 1)
 
 # %%
-new_hashes_polyvore = {1: apparel_feature_vecs}
-new_hashes_polyvore
-
-# %%
 new_storage_path = "../../storages/hash_apparels_chatbot.pkl"
 new_storage_path
 
 # %%
-# io.save_pickle(new_hashes_polyvore, new_storage_path)
+io.save_pickle(apparel_feature_vecs, new_storage_path)
 new_hashes_polyvore = io.load_pickle(new_storage_path)
-len(new_hashes_polyvore[1])
+len(new_hashes_polyvore)
+
+# # %%
+# new_hashes_polyvore = {1: apparel_feature_vecs}
+# new_hashes_polyvore
+# io.save_pickle(new_hashes_polyvore, new_storage_path)
 
 # %% [markdown]
 ## Fashion CLIP
