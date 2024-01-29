01bd8a334891000099de22a20057ed5f3c2ec8c6
dungnq271 <dungnguyen2712002@gmail.com>
2024-01-29 17:29:28 +0700

    Rename files to run

--- runs/preprocess/fclip_embedding.sh
+++ runs/preprocess/fclip_embedding.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-python tools/fclip_extract_embeddings.py \
+python src/tools/fclip_extract_embeddings.py \
   	 -p data/polyvore/item_cates.csv \
 	 -d data/polyvore/images \
 	 -s data/polyvore/embeddings/fclip_embeddings.txt
