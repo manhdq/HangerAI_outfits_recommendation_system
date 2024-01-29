1d73201c889bca01517aab61fb1fd99958ad8eb3
dungnq271 <dungnguyen2712002@gmail.com>
2024-01-29 15:56:22 +0700

    Add pending updated path operation function

--- src/api.py
+++ src/api.py
@@ -14,3 +14,16 @@ model = get_outfit_recommend(config_path)
 @app.post("/outfits_recommend_from_prompt/", status_code=200)
 async def outfit_recommend_from_prompt(prompt: TextInput):
     return model.outfits_recommend_from_prompt(prompt.text)
+
+
+## TODO
+# async def outfit_recommend_from_prompt(data_input: ApparelsInput):
+#     prompt = data_input.text
+#     item_names = data_input.name
+#     input64s = data_input.input64
+
+#     storage = {}
+#     for name, inp64 in zip(item_names, input64s):
+#         image = base64_to_image(inp64)
+
+#     return model.outfits_recommend_from_prompt(prompt.text)
