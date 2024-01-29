7c27b49c8c4b441379015ca7e72a09a82bfabc60
dungnq271 <dungnguyen2712002@gmail.com>
2024-01-29 17:29:48 +0700

    Add new path operation function

--- src/api.py
+++ src/api.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+from .schemas import ApparelsInput
 from .pipeline import get_outfit_recommend
 
 class TextInput(BaseModel):
@@ -8,6 +9,7 @@ class TextInput(BaseModel):
 app = FastAPI()
 
 config_path = "configs/polyvore_outfit_recommend.yaml"
+fhn_config_path = "configs/FHN_VOE_T3_fashion32.yaml"
 model = get_outfit_recommend(config_path)
 
 
@@ -17,13 +19,18 @@ async def outfit_recommend_from_prompt(prompt: TextInput):
 
 
 ## TODO
-# async def outfit_recommend_from_prompt(data_input: ApparelsInput):
-#     prompt = data_input.text
-#     item_names = data_input.name
-#     input64s = data_input.input64
+@app.post("/outfits_recommend_from_inputs/", status_code=200)
+async def outfit_recommend_from_inputs(
+    # data_input: ApparelsInput
+    prompt: TextInput
+):
+    # prompt = data_input.text
+    # item_names = data_input.name
+    # input64s = data_input.input64
 
-#     storage = {}
-#     for name, inp64 in zip(item_names, input64s):
-#         image = base64_to_image(inp64)
-
-#     return model.outfits_recommend_from_prompt(prompt.text)
+    return model.outfits_recommend_from_inputs(
+        fhn_config_path,
+        prompt,
+        # item_names,
+        # input64s
+    )
